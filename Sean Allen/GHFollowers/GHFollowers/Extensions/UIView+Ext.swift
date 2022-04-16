4462da465db7688019ae2610f87a6c0d4deb3a07
Fabricio Pujol <Fabricio_2112@hotmail.com>
2022-04-16 16:00:16 -0300

    fixing project for iPhoneSE

--- Sean Allen/GHFollowers/GHFollowers/Extensions/UIView+Ext.swift
+++ Sean Allen/GHFollowers/GHFollowers/Extensions/UIView+Ext.swift
@@ -8,6 +8,18 @@
 import UIKit
 
 extension UIView {
+
+    func pinToEdges(of superview: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superview.topAnchor),
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor),
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor)
+        ])
+    }
+
     func addSubviews(_ views: UIView...) {
         for view in views { addSubview(view) }
     }
